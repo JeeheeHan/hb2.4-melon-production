88d939abdb22c56a4a2c63cdde3b65cc0ac4237f
Jenny Han <han.jennyjh@outlook.com>
2021-03-04 04:48:12 +0000

    First version using choice to pick out a random winner

--- melon-raffle/version2/raffle.py
+++ melon-raffle/version2/raffle.py
@@ -1,6 +1,31 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+import customers 
+from random import choice
+import sys
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+def get_random_customer(customers):
+    """
+    Choice a person from customer list 
+    """
+    lucky_customer = choice(customers)
+
+    print (f"Name: {lucky_customer.name}")
+    print (f"Email: {lucky_customer.email}")
+    print (f"Street: {lucky_customer.street}")
+    print (f"City: {lucky_customer.city}")
+    print (f"Zipcode: {lucky_customer.zipcode}")
+
+
+
+
+
+#call document in shell
+
+filenames = "customers.txt"
+#sys.argv[1:] was not working so putting direct file name
+
+#Return a lucky customer
+lst_customers = customers.get_customers_from_file(filenames)
+
+winner = get_random_customer(lst_customers)
